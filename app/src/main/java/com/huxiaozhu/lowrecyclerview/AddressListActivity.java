bd9d11375f01689c4c1d630f4d6182bcc5856110
liusmallpig <chenhui.hu@carota.ai>
2019-04-11 09:46:38 +0800

    修改布局文件

--- app/src/main/java/com/huxiaozhu/lowrecyclerview/AddressListActivity.java
+++ app/src/main/java/com/huxiaozhu/lowrecyclerview/AddressListActivity.java
@@ -1,7 +1,9 @@
 package com.huxiaozhu.lowrecyclerview;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * 通讯录顶吸
